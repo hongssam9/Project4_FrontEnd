0984bb151505c4c01f179d249bb1351751e2fab3
tadsmyth <tadsmith7@gmail.com>
2022-01-20 14:48:00 -0600

    upload page moved to new comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Route} from "react-router-dom";
 import { Container, Navbar, Nav, Button } from "react-bootstrap";
 import Login from "./components/Login.jsx";
-import ImageFeed from "./components/ImageFeed.jsx";
+import ImageFeed from './components/Imagefeed'
 import Splash from "./components/Splash.jsx";
 import Settings from "./components/Settings.jsx";
 import Signup from "./components/Signup.jsx";
@@ -18,54 +18,6 @@ import { BsFillPersonFill } from 'react-icons/bs'
 import axios from 'axios';
 
 class App extends React.Component {
- 
-  state = {
-      image: null,
-      description : [],
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  };
-
-  handleImageChange = (e) => {
-    this.setState({
-      image: e.target.files[0]
-    })
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(this.state);
-    let form_data = new FormData();
-    form_data.append('image', this.state.image);
-    form_data.append('description', this.state.description);
-    let url = 'http://shtsxh.herokuapp.com/';
-    axios.post(url, form_data, {
-      headers: {
-        'content-type': 'multipart/form-data'          // what is this?
-      }
-    })
-        .then(res => {
-          console.log(res.data);
-        })
-        .catch(err => console.log(err))
-  };
-
-
-  componentDidMount() {
-      let data ;
-      axios.get('http://shtsxh.herokuapp.com/')
-      .then(res => {
-          data = res.data;
-          this.setState({
-              details : data    
-          });
-      })
-      .catch(err => {})
-  }
 render() {
   return(
     <div className = "App">
@@ -110,25 +62,9 @@ render() {
             <Nav className="me-auto">
               <Nav.Link className = "mx-5" href="/"><RiHome3Fill size={25} /></Nav.Link>
            
-              <Button className = "mx-4" variant = "info" /* href="/UploadImages" */> 
-                <AiOutlinePlus size={27}/>
-                <form onSubmit={this.handleSubmit}>
-          <p>
-            <input type="text" placeholder='Title' id='title' value={this.state.title} onChange={this.handleChange} required/>
-          </p>
-          <p>
-            <input type="text" placeholder='Content' id='content' value={this.state.content} onChange={this.handleChange} required/>
-
-          </p>
-          <p>
-            <input type="file"
-                   id="image"
-                   accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
-          </p>
-          <input type="submit"/>
-        </form>
+              <Button className = "mx-4" variant = "info" href="/UploadImages"> 
+                <AiOutlinePlus size={27}/> 
               </Button>
-             
               <Nav.Link className = "mx-5" href="/Userprof"><BsFillPersonFill size={27}/></Nav.Link>
             </Nav>
           </Container>
